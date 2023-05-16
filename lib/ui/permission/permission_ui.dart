7df0c723abce18d8c3a768b0635af184334d27f2
Novian Andika <novian.andika@students.amikom.ac.id>
2023-05-16 16:44:00 +0700

    🚀 feat(router.dart): add route for permission page 🐛 fix(homo.entities.dart): fix typo in title of permission button ✨ feat(permission_ui.dart): add permission page UI The router now has a new route for the permission page, which allows users to navigate to the page. The title of the permission button in the home screen has been fixed to correct the typo. The permission page UI has been added, which displays a centered text widget with the title "Ijin".

---
+++ lib/ui/permission/permission_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PermissionPage extends StatelessWidget {
+  const PermissionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ijin'),
+      ),
+      body: const Center(
+        child: Text('Ijin'),
+      ),
+    );
+  }
+}
